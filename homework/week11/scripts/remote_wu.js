ae9cfe9f46f21678b736e653982f2003e034ba8b
tho12038 <martin.abrt.thomas@gmail.com>
2016-11-22 16:30:53 -0700

    spelling error

--- homework/week11/scripts/remote_wu.js
+++ homework/week11/scripts/remote_wu.js
@@ -26,14 +26,14 @@ $(function () {
 url : "https://api.wunderground.com/api/9dcea6730ca4d03e/geolookup/conditions/q/" + lat + "," + long + ".jsonp",       
   dataType : "jsonp",
   success : function(parsed_json) {
-console.log(parsed_json);
+  console.log(parsed_json);
   var city = parsed_json['location']['city'];
   var state = parsed_json['location']['state'];
   var temp_f = Math.round(parseInt(parsed_json['current_observation']['temp_f']));
   var state = parsed_json['location']['state'];
   var summary = parsed_json["current_observation"]["weather"];
   var humidity = parsed_json['current_observation']['relative_humidity'];
-  var wind = parsed_json['currnet_observation']['wind_mph'];
+  var wind = parsed_json['current_observation']['wind_mph'];
   
   $("#cityDisplay").text(city + ", " + state);
   $("#summary").text(summary);
