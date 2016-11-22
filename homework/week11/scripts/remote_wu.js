48cc75e6f2bbedd33528007f634f364e5f64809a
tho12038 <martin.abrt.thomas@gmail.com>
2016-11-22 16:39:33 -0700

    Feels Like

--- homework/week11/scripts/remote_wu.js
+++ homework/week11/scripts/remote_wu.js
@@ -34,13 +34,14 @@ url : "https://api.wunderground.com/api/9dcea6730ca4d03e/geolookup/conditions/q/
   var summary = parsed_json["current_observation"]["weather"];
   var humidity = parsed_json['current_observation']['relative_humidity'];
   var wind = parsed_json['current_observation']['wind_mph'];
+  var feels = parsed_json['current_observation']['feelslike_f'];
   
   $("#cityDisplay").text(city + ", " + state);
   $("#summary").text(summary);
   $("#currentTemp").text(temp_f + "°");
   $("#add1").text("Humidity:" + " " + humidity);
-  $("#add2").text("Current Wind Spped is: " + " " + wind + "MPH");
-  
+  $("#add2").text("Current Wind Spped is: " + wind + " " "MPH");
+  $("#add3").text("Feels Like: " + feels + "°")
                
                 
       $("#cover").fadeOut(250);
