1efd43b7860a35c94397138b25da9a6cbd3f593a
tho12038 <martin.abrt.thomas@gmail.com>
2016-11-25 21:37:50 -0700

    weather current

---
+++ homework/weatherModules.js
@@ -0,0 +1,7 @@
+// Include modules into the html file
+// Pathes may need altered depending on site structure
+$(function () {
+  $("#page-header").load("/homework/weatherHeader.html");
+  $("#page-nav").load("/homework/weatherNav.html");
+  $("#footer-content").load("/homework/weatherFooter.html");
+});
