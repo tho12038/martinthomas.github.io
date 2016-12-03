6498909ee7306d7ed2fe561dec551ea2a1710423
tho12038 <martin.abrt.thomas@gmail.com>
2016-12-03 12:23:56 -0700

    weather search

---
+++ final/scripts/modules.js
@@ -0,0 +1,5 @@
+$(function () {
+  $("#page-header").load("/homework/week11/modules/header.html");
+  $("#page-nav").load("/final/modulesFinal/navFinal.html");
+  $("#footer-content").load("/homework/week11/modules/footer.html");
+});
